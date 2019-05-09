cb7254d4b0de81db88b073ae21c6b5fb8a3ab459
marcoemidio <marcoemidio@users.noreply.github.com>
2019-05-09 16:05:23 +0100

    initial commit

---
+++ soap-service/src/main/java/pt/cgd/endpoint/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package pt.cgd.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import pt.cgd.repository.CountryRepository;
+import pt.cgd.schemas.countries.country_web_service.GetCountryRequest;
+import pt.cgd.schemas.countries.country_web_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://cgd.pt/schemas/countries/country-web-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
