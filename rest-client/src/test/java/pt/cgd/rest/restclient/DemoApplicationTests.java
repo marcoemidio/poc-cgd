51c945223884cfd3383a9cc039f7db8580f1f0b3
marco-emidio <marco.emidio@timestamp.pt>
2019-07-17 17:32:54 +0100

    added rest-service

---
+++ rest-client/src/test/java/pt/cgd/rest/restclient/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.cgd.rest.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
